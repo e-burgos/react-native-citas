febc2f6aec900538f5e8859e01078dbb6d179a20
Esteban Burgos <estebanburgos85@gmail.com>
2021-05-14 17:08:45 -0300

    Dates component added

--- App.js
+++ App.js
@@ -6,14 +6,76 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+import Dates from './components/Dates';
 
 const App = () => {
+
+  // Definir state de citas
+  const [dates, setDates] = useState([
+    { id: 1, pet: "Nuva", owner: "Esteban", symptom: "No come"},
+    { id: 2, pet: "Regi", owner: "Juan", symptom: "No duerme"},
+    { id: 3, pet: "Maria", owner: "Pedro", symptom: "No canta"}
+  ]);
+
+  const filterDates = (id) => {
+    setDates( (dates) => {
+      return dates.filter( date => date.id !== id)
+    })
+  }
   
   return (
-    <Text>Proyecto Citas</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Administrador de Citas</Text>
+
+      { dates.length == 0 ? 
+      <View style={styles.infoText}>
+        <Text style={styles.infotitle}>No hay citas, empieza creando una.</Text>
+      </View>
+      : null}
+      
+
+      <FlatList 
+        data={dates}
+        renderItem={ ({item}) => <Dates date={item} filterDates={filterDates}/> }
+        keyExtractor={ date => date.id }
+      />
+
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  
+  container: {
+    backgroundColor: '#4B8DF5',
+    flex: 1
+  },
+  title: {
+    marginTop: 50,
+    marginBottom: 10,
+    color: 'white',
+    textTransform: 'uppercase',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  infotitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white'
+  },
+  infoText: {
+    marginTop: 50,
+    backgroundColor: 'red',
+    marginVertical: 5,
+    marginHorizontal: 15,
+    borderRadius: 5,
+    paddingHorizontal: 20,
+    paddingVertical: 20
+    },
+});
 
 export default App;
---
+++ components/Dates.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Text, StyleSheet, View, TouchableHighlight } from 'react-native'
+
+const Dates = ({date, filterDates}) => {
+
+    const destroyDate = (id) => {
+        console.log('Eliminando...' + id)
+        filterDates(id);
+    }
+
+    return ( 
+        <View style={styles.dateContainer}>
+            <View style={styles.dateLabel}>
+                <Text style={styles.label}>Mascota:</Text>
+                <Text style={styles.text}>{date.pet}</Text>
+            </View>
+            <View style={styles.dateLabel}>
+                <Text style={styles.label}>Propieratio:</Text>
+                <Text style={styles.text}>{date.owner}</Text>
+            </View>
+            <View style={styles.dateLabel}>
+                <Text style={styles.label}>Síntomas:</Text>
+                <Text style={styles.text}>{date.symptom}</Text>
+            </View>
+            <View>
+                <TouchableHighlight onPress={() => destroyDate(date.id)} style={styles.button}>
+                    <Text style={styles.buttonText}>&times; Eliminar</Text>
+                </TouchableHighlight>
+            </View>
+        </View>
+     );
+}
+
+const styles = StyleSheet.create({
+    dateContainer: {
+        backgroundColor: 'white',
+        marginVertical: 5,
+        marginHorizontal: 15,
+        borderRadius: 5,
+        paddingHorizontal: 20,
+        paddingVertical: 20
+    },
+    dateLabel: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 10
+    },
+    label: {
+        fontWeight: 'bold',
+        fontSize: 18,
+    },
+    text: {
+        fontSize: 18,
+        marginLeft: 4
+    },
+    button: {
+        backgroundColor: 'red',
+        borderRadius: 5,
+        padding: 10,
+        marginTop: 20
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    }
+})
+ 
+export default Dates;
