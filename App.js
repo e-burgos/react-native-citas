96d00dbd0008f50c7ad5f34ce1cc06657f1f39ac
Esteban Burgos <estebanburgos85@gmail.com>
2021-05-14 13:32:46 -0300

    Initialized project

---
+++ App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Text } from 'react-native';
+
+const App = () => {
+  
+  return (
+    <Text>Proyecto Citas</Text>
+  );
+};
+
+export default App;
